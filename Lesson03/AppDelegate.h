6dd1236fe902064d040e2e62c18551a9faae13a8
user <user@users-MacBook-Pro-2.local>
2017-07-07 10:26:42 +0300

    Initial Commit

---
+++ Lesson03/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson03
+//
+//  Created by user on 7/7/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson03/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Lesson03
+//
+//  Created by user on 7/7/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
